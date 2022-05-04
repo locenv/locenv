94eeb84979c5cad64176b7137a829eb0839a7bb9
Putta Khunchalee <ultimaweapon@outlook.com>
2022-05-04 16:53:18 +0700

    Adds clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use clap::{command, Command};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Parse arguments.
+    let args = command!()
+        .subcommand(
+            Command::new("up").about("Start all services"),
+        )
+        .get_matches();
+
+    // Handle sub-commands.
+    if let Some(up) = args.subcommand_matches("up") {
+    }
+
     println!("Hello, world!");
     Ok(())
 }
