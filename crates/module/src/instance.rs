103db46a404c759ee98c318403351ee15805cca2
Putta Khunchalee <ultimaweapon@outlook.com>
2022-07-02 11:36:23 +0700

    Adds more APIs

--- crates/module/src/instance.rs
+++ crates/module/src/instance.rs
@@ -1,9 +1,25 @@
 use libloading::{Library, Symbol};
+use lua::{__va_list_tag, lua_CFunction, lua_Integer, lua_Number, lua_State, size_t};
 use std::error::Error;
+use std::ffi::c_void;
 use std::fmt::{Display, Formatter};
+use std::os::raw::{c_char, c_int};
 use std::path::{Path, PathBuf};
 
-const API_TABLE: ApiTable = ApiTable {};
+const API_TABLE: ApiTable = ApiTable {
+    lua_pushboolean: lua::lua_pushboolean,
+    lua_pushcclosure: lua::lua_pushcclosure,
+    lua_pushfstring: lua::lua_pushfstring,
+    lua_pushinteger: lua::lua_pushinteger,
+    lua_pushlightuserdata: lua::lua_pushlightuserdata,
+    lua_pushlstring: lua::lua_pushlstring,
+    lua_pushnil: lua::lua_pushnil,
+    lua_pushnumber: lua::lua_pushnumber,
+    lua_pushstring: lua::lua_pushstring,
+    lua_pushthread: lua::lua_pushthread,
+    lua_pushvalue: lua::lua_pushvalue,
+    lua_pushvfstring: lua::lua_pushvfstring,
+};
 
 pub struct Instance {
     library: Library,
@@ -20,7 +36,21 @@ pub enum BootstrapError {
 }
 
 #[repr(C)]
-struct ApiTable {}
+struct ApiTable {
+    lua_pushboolean: unsafe extern "C" fn(*mut lua_State, c_int),
+    lua_pushcclosure: unsafe extern "C" fn(*mut lua_State, lua_CFunction, c_int),
+    lua_pushfstring: unsafe extern "C" fn(*mut lua_State, *const c_char, ...) -> *const c_char,
+    lua_pushinteger: unsafe extern "C" fn(*mut lua_State, lua_Integer),
+    lua_pushlightuserdata: unsafe extern "C" fn(*mut lua_State, *mut c_void),
+    lua_pushlstring: unsafe extern "C" fn(*mut lua_State, *const c_char, size_t) -> *const c_char,
+    lua_pushnil: unsafe extern "C" fn(*mut lua_State),
+    lua_pushnumber: unsafe extern "C" fn(*mut lua_State, lua_Number),
+    lua_pushstring: unsafe extern "C" fn(*mut lua_State, *const c_char) -> *const c_char,
+    lua_pushthread: unsafe extern "C" fn(*mut lua_State) -> c_int,
+    lua_pushvalue: unsafe extern "C" fn(*mut lua_State, c_int),
+    lua_pushvfstring:
+        unsafe extern "C" fn(*mut lua_State, *const c_char, *mut __va_list_tag) -> *const c_char,
+}
 
 // Instance
 
